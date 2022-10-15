21ceb9c573b2bfef9b080de9c2b8f9d7811b2ceb
Joinemm <joonas@rautiola.co>
2022-10-15 18:20:04 +0300

    Fix footer link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,26 +2,26 @@ import './Footer.css'
 import AvainLippuWhite from '../../images/avainlippu_white.webp'
 import AvainLippuBlue from '../../images/avainlippu_blue.webp'
 
-const Footer = ({ theme }) => (
-  <footer className='footer'>
-    <div className='footer__inner'>
-      <div className='footer__flag'>
-        <img
-          src={theme === 'dark' ? AvainLippuWhite : AvainLippuBlue}
-          alt='Made in Finland'
-          width={100}
-        />
+const Footer = ({ theme }) => {
+  const footerLink = 'https://github.com/joinemm'
+  return (
+    <footer className='footer'>
+      <div className='footer__inner'>
+        <div className='footer__flag'>
+          <img
+            src={theme === 'dark' ? AvainLippuWhite : AvainLippuBlue}
+            alt='Made in Finland'
+            width={100}
+          />
+        </div>
+        <div className='footer__center'>
+          <a href={footerLink} className='link footer__link'>
+            Created by Joinemm#7184
+          </a>
+        </div>
       </div>
-      <div className='footer__center'>
-        <a
-          href='https://github.com/rajshekhar26/cleanfolio'
-          className='link footer__link'
-        >
-          Created by Joinemm#7184
-        </a>
-      </div>
-    </div>
-  </footer>
-)
+    </footer>
+  )
+}
 
 export default Footer
